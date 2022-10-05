e649bd2597c01206450dc320c4c276b1685c7e37
Francisco Munoz <franciscomunozcontato@gmail.com>
2022-10-04 21:52:04 -0300

    Elaboração do dropdown no menu hamburguer.

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -17,3 +17,23 @@ $(document).ready(function() {
            event.stopPropagation();
        });
    });
+
+   $(document).ready(function() {
+    var botao = $('.nav-features');
+    var dropDown = $('.features-list-mobile');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.nav-company');
+    var dropDown = $('.company-list-mobile');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
